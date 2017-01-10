3e7ff95a2283a7557161104cf1ae9d988c79ef09
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-01-10 18:12:38 +0900

    temp save

--- Lighting.cpp
+++ Lighting.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <math.h>
 #include <iostream>
+#include <string.h>
 #include <map>
 #include <sstream>
 #include <vector>
@@ -64,22 +65,22 @@ struct P {
   }
 };
 
-class Wall {
+bool boundBoxIntersect(ll a, ll b, ll c, ll d) {
+  return max(min(a,b), min(c,d)) <= min(max(a,b), max(c,d));
+}
+
+ll orientedAreaSign(P a, P b, P c) {
+  int area = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+  return area == 0 ? 0 : area / abs(area);
+}
+
+struct Wall {
   P start;
   P end;
 
   Wall(P st, P e) {
     start = st;
     end = e;
-  }
-
-  bool boundBoxIntersect(ll a, ll b, ll c, ll d) {
-    return max(min(a,b), min(c,d)) <= min(max(a,b), max(c,d));
-  }
-
-  ll orientedAreaSign(P a, P b, P c) {
-    int area = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-    return area == 0 ? 0 : area / abs(area);
   }
 
   bool intersect(Wall other) {
@@ -91,18 +92,47 @@ class Wall {
 };
 
 ll g_LightDistance;
+ll g_LightCount;
 ll g_width;
 ll g_height;
+int S;
+vector<Wall> g_walls;
+vector<P> g_lights;
 
 class Lighting {
   public:
+    void extractWalls(vector<string> map) {
+
+      for (int i = 0; i < S-1; i++) {
+        int j = 0;
+
+        while (j < S) {
+          if (map[i][j] == map[i+1][j]) {
+            j++;
+            continue;
+          }
+
+          P start(j*2*f, (i+1)*2*f);
+          while (j < S && map[i][j] != map[i+1][j]) {
+            j++;
+          }
+          P end(j*2*f, (i+1)*2*f);
+          Wall w(start, end);
+          g_walls.push_back(w);
+        }
+      }
+    }
+
     vector<string> setLights(vector<string> map, int D, int L) {
       vector<string> ret;
       vector<P> points;
-
       g_LightDistance = D;
+      g_LightCount = L;
+      S = map.size();
       g_height = map.size();
       g_width = map[0].size();
+
+      extractWalls(map);
 
       srand(123);
       int S = map.size();
@@ -114,13 +144,71 @@ class Lighting {
       return ret;
     }
 
-    void markPointsIlluminated(P p) {
-      int boxX1 = max(0LL, p.x - g_LightDistance);
-      int boxX2 = min(g_width-1, p.x + g_LightDistance);
-      int boxY1 = max(0LL, p.y - g_LightDistance);
-      int boxY2 = min(g_height-1, p.y + g_LightDistance);
+    double calcScore(vector<string> map) {
+      vector<vector<int> > points(S, vector<int>(S, 0));
+
+      for (int i = 0; i < g_LightCount; i++) {
+        markPointsIlluminated(i, points);
+      }
+
+      int nIllum = 0;
+      int nTotal = 0;
+
+      for (int r = 0; r < S; r++) {
+        for (int c = 0; c < S; c++) {
+          if (map[r][c] == '#') continue;
+
+          for (int x = 0; x < f; x++) {
+            for (int y = 0; y < f; y++) {
+              nTotal++;
+              if (points[r*f+y][c*f+x] > 0) {
+                nIllum++;
+              }
+            }
+          }
+        }
+      }
+
+      return nIllum * 1.0 / nTotal;
+    }
+
+    void markPointsIlluminated(int lightInd, vector<vector<int> > &points) {
+      P light = g_lights[lightInd];
+
+      int boxX1 = max(0LL, light.x - g_LightDistance);
+      int boxX2 = min(g_width-1, light.x + g_LightDistance);
+      int boxY1 = max(0LL, light.y - g_LightDistance);
+      int boxY2 = min(g_height-1, light.y + g_LightDistance);
 
       vector<int> localWallsInd;
+      for (int i = 0; i < g_walls.size(); i++) {
+        Wall w = g_walls[i];
+        if (boundBoxIntersect(boxX1, boxX2, w.start.x, w.end.x) &&
+            boundBoxIntersect(boxY1, boxY2, w.start.y, w.end.y)) {
+              localWallsInd.push_back(i);
+            }
+      }
+
+      for (int x = (int)boxX1 / 2; x <= boxX2 / 2; x++) {
+        for (int y = (int)boxY1 / 2; y <= boxY2 / 2; y++) {
+          if (points[y][x] != 0) {
+            continue;
+          }
+          P point(x*2+1, y*2+1);
+          if (!light.near(point, g_LightDistance)) continue;
+          bool ok = true;
+          Wall beam(point, light);
+          for (int ind : localWallsInd) {
+            if (beam.intersect(g_walls[ind])) {
+              ok = false;
+              break;
+            }
+          }
+          if (ok) {
+            points[y][x] = 1 + lightInd;
+          }
+        }
+      }
     }
 };
 
